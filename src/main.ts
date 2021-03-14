4cc60bf799f12d7fba675a9719934347311c02d3
Lucas Guiss <lucasguissgusmao@gmail.com>
2021-03-14 10:54:36 -0300

    feat(bootstrap): add bootstrap plugin to vue3

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import './plugins/bootstrap-vue';
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+
 import { createApp } from 'vue';
 import i18n from '@/i18n';
 import App from '@/App.vue';
