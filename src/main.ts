4e7c978c9f01e02113b385e88071c34954df6ec9
Lucas Guiss <lucasguissgusmao@gmail.com>
2021-03-14 11:13:38 -0300

    feat(style): add element plus

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import i18n from '@/i18n';
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
+import ElementPlus from 'element-plus/';
+import 'element-plus/lib/theme-chalk/index.css';
 
 createApp(App).use(store).use(router).use(i18n)
+  .use(ElementPlus)
   .mount('#app');
