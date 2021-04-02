d32f910e2508baccf64719b400458d3ccf3d6198
Lucas Guiss <lucas.gusmao@roit.com.br>
2021-04-02 14:46:18 -0300

    feat(views): add all three basic views

---
+++ src/views/AboutMe.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <Header />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from '@/components/Header.vue';
+
+export default defineComponent({
+  name: 'AboutMe',
+  components: {
+    Header,
+  },
+});
+</script>
+<style scoped>
+.about {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="projects">
+    <Header />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from '@/components/Header.vue';
+
+export default defineComponent({
+  name: 'Projects',
+  components: {
+    Header,
+  },
+});
+</script>
+<style scoped>
+.projects {
+  width: 100%;
+  height: 100%;
+}
+</style>
